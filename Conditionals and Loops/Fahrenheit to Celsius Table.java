e50e24276297897c473325792aa5f41ddfc236f7
Pratyush_ <109838491+PratyushKumarDehury@users.noreply.github.com>
2022-07-30 13:29:52 +0530

    Update Fahrenheit to Celsius Table.java

--- Conditionals and Loops/Fahrenheit to Celsius Table.java
+++ Conditionals and Loops/Fahrenheit to Celsius Table.java
@@ -1,8 +1,10 @@
-Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
+Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), 
+you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
 Input Format :
 3 integers - S, E and W respectively 
 Output Format :
-Fahrenheit to Celsius conversion table. One line for every Fahrenheit and corresponding Celsius value. The Fahrenheit value and its corresponding Celsius value should be separate by single space.
+Fahrenheit to Celsius conversion table. One line for every Fahrenheit and corresponding Celsius value.
+The Fahrenheit value and its corresponding Celsius value should be separate by single space.
 Constraints :
 0 <= S <= 90
 S <= E <=  900
@@ -32,7 +34,11 @@ Sample Output 2:
 98  36
 111 43
 Explanation For Input 2:
-Start calculating the Celsius values for each Fahrenheit Value which starts from 20. So starting from 20, we need to compute its corresponding Celsius value which computes to -6. We print this information as <Fahrenheit Value> <a single space> <Celsius Value> on each line. Now add 13 to Fahrenheit Value at each step until you reach 119 in this case. You may or may not exactly land on the end value depending on the steps you are taking.
+Start calculating the Celsius values for each Fahrenheit Value which starts from 20. 
+So starting from 20, we need to compute its corresponding Celsius value which computes to -6. 
+We print this information as <Fahrenheit Value> <a single space> <Celsius Value> on each line. 
+Now add 13 to Fahrenheit Value at each step until you reach 119 in this case. 
+You may or may not exactly land on the end value depending on the steps you are taking.
 
 import java.util.Scanner;
 public class Solution {
