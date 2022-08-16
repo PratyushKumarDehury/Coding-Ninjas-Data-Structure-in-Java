a15427188efbf6f997e8256cbf1d9b78d4c17851
Pratyush_ <109838491+PratyushKumarDehury@users.noreply.github.com>
2022-08-16 08:37:06 +0530

    Number Star pattern 1.java

---
+++ Test1/Number Star pattern 1.java
@@ -0,0 +1,36 @@
+Print the following pattern for given number of rows.
+Input format :
+Integer N (Total number of rows)
+Output Format :
+Pattern in N lines
+Sample Input :
+   5
+Sample Output :
+ 5432*
+ 543*1
+ 54*21
+ 5*321
+ *4321
+
+
+import java.util.Scanner;
+public class runner {
+	public static void main(String[] args) {
+		// Write your code here
+        
+        Scanner s = new Scanner(System.in);
+        int n,i,j;
+        n = s.nextInt();
+        for(i = 1; i <= n; i++)
+        {
+            for(j = n; j > 0; j--){
+                if(j == i)
+                    System.out.print("*");
+                else
+                    System.out.print(j);
+            }
+            System.out.println();
+        }
+
+	}
+}
