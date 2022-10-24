88f4b5e89f9f3eedeab70c468ac50cbb2f86f995
Pratyush_ <109838491+PratyushKumarDehury@users.noreply.github.com>
2022-10-24 17:59:41 +0530

    First Index Of a Number in an Array  Question.java

---
+++ Recursion 1/First Index Of a Number in an Array  Question.java
@@ -0,0 +1,46 @@
+Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
+First index means, the index of first occurrence of x in the input array.
+Do this recursively. Indexing in the array starts from 0.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+first index or -1
+
+Constraints :
+1 <= N <= 10^3
+Sample Input :
+4
+9 8 10 8
+8
+Sample Output :
+1
+
+
+
+
+
+
+public class Solution {
+    public static int helper(int input[], int x, int index){
+        
+     if(index > input.length - 1)
+        return -1;  
+        
+
+        if(input[index] == x)
+            return index;
+        return helper(input,x,index + 1);
+    }
+
+	public static int firstIndex(int a[], int x) {
+        
+        return helper(a, x, 0);
+    }
+}
+
+
+
+
+
