a3b1961adf9c77515b71de60d00bd8c0ec99697a
Pratyush_ <109838491+PratyushKumarDehury@users.noreply.github.com>
2022-11-18 12:29:08 +0530

    Multiplication (Recursive).java

--- Recursion 2/Multiplication (Recursive).java
+++ Recursion 2/Multiplication (Recursive).java
@@ -29,7 +29,7 @@ Sample Output 2 :
 public class solution {
 
 	public static int multiplyTwoIntegers(int m, int n){
-		// Write your code here
+	// Write your code here
         if(n==0)
         return 0;
         return m+multiplyTwoIntegers(m,n-1);
