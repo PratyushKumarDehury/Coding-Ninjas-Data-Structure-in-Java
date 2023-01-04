c9100a44cd7828de250db71dc76b3bcbb3728b8d
Pratyush_ <109838491+PratyushKumarDehury@users.noreply.github.com>
2023-01-04 13:05:12 +0530

    Reverse LL (Recursive).java

---
+++ Linked List 2/Reverse LL (Recursive).java
@@ -0,0 +1,108 @@
+Given a singly linked list of integers, reverse it using recursion and return the head to the modified list. You have to do this in O(N) time complexity where N is the size of the linked list.
+ Note :
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements of the singly linked list separated by a single space.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format :
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a seperate line.
+  
+ Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^4
+Where M is the size of the singly linked list.
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+1 2 3 4 5 6 7 8 -1
+Sample Output 1 :
+8 7 6 5 4 3 2 1
+Sample Input 2 :
+2
+10 -1
+10 20 30 40 50 -1
+Sample Output 2 :
+10 
+50 40 30 20 10 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /*
+
+    Following is the Node class already written for the Linked List
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+    
+        public LinkedListNode(T data) {
+            this.data = data;
+        }
+    }
+
+*/
+
+public class Solution {
+
+	public static LinkedListNode<Integer> reverseLinkedListRec(LinkedListNode<Integer> head) {
+		    //Your code goes here
+        //base case
+        //Time complexity -> O(n^2)
+//         if(head == null || head.next == null)
+//             return head;
+//         LinkedListNode<Integer> temp =  reverseLinkedListRec(head.next);
+//         LinkedListNode<Integer> temp2 = temp;
+//         while(temp2.next != null)
+//             temp2 = temp2.next;
+//         	temp2.next = head;
+//         	head.next = null;
+//         return temp;
+//     }
+// }
+
+
+        //Time complexity -> O(n)
+        //recursive method
+        if(head == null || head.next == null)
+            return head;
+        LinkedListNode<Integer> temp =  reverseLinkedListRec(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+}
+        
+        
+        //iterative method
+        // I/P a->b->c->\0
+        // o/p c->b->a->\0
+        //Taking 3 pointer
+        //base case
+    //     if(head == null)
+    //         return head;
+    //     LinkedListNode<Integer> curr = head;
+    //     LinkedListNode<Integer> prev = null;
+    //     LinkedListNode<Integer> ne = null;//head.next;
+    //     while(curr != null){
+    //     ne =  curr.next; //most optomized line.
+    //     curr.next = prev;
+    //     prev = curr;
+    //     curr = ne;
+    //     }
+    //     return prev;
+    // }
+//}
+
+        
