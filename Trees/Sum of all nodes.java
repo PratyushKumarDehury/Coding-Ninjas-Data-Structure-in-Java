46aef38bb4efa6d2367aec76f4e08b21666e914e
Pratyush Kumar Dehury <109838491+PratyushKumarDehury@users.noreply.github.com>
2023-02-13 08:28:11 +0530

    Sum of all nodes.java

--- Trees/Sum of all nodes.java
+++ Trees/Sum of all nodes.java
@@ -33,9 +33,9 @@ public class Solution {
 	
 	
 	public static int sumOfAllNode(TreeNode<Integer> root){
-       int sum = root.data;
-		 for(int i = 0; i < root.children.size(); i++){
-			 sum += sumOfAllNode(root.children.get(i));
+       		int sum = root.data;
+		for(int i = 0; i < root.children.size(); i++){
+		    sum += sumOfAllNode(root.children.get(i));
 		 }
 		 return sum;
 	}
