922e079ffde8c5a0a6f602de6faa15de967abfbd
Pratyush Kumar Dehury <109838491+PratyushKumarDehury@users.noreply.github.com>
2023-02-13 08:33:09 +0530

    Update Code : Number of nodes greater than x.java

--- Trees/Code : Number of nodes greater than x.java
+++ Trees/Code : Number of nodes greater than x.java
@@ -20,8 +20,7 @@ Sample Output 2:
   
   
   
-  
-  import java.util.Scanner;
+import java.util.Scanner;
 public class Solution {
 
 /*	TreeNode class 
@@ -40,15 +39,15 @@ public class Solution {
 	
 	public static int numNodeGreater(TreeNode<Integer> root,int x){
 
-		// Write your code here    
-    if(root == null){
-			return 0;
-		}else if(root.data > x){
-			count++;
-		}
-		for(int i = 0; i < root.children.size(); i++){
-			numNodeGreater(root.children.get(i), x);
-		}
+	// Write your code here    
+        if(root == null){
+	  return 0;
+	}else if(root.data > x){
+	  count++;
+	}
+	for(int i = 0; i < root.children.size(); i++){
+	    numNodeGreater(root.children.get(i), x);
+	}
         return count;
 
 	}	
