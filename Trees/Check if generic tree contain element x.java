798ae8a2dd53b21459a59f7face09f443d6d7d7d
Pratyush Kumar Dehury <109838491+PratyushKumarDehury@users.noreply.github.com>
2023-02-15 17:50:58 +0530

    Update Check if generic tree contain element x.java

--- Trees/Check if generic tree contain element x.java
+++ Trees/Check if generic tree contain element x.java
@@ -43,20 +43,20 @@ public class Solution {
 			children = new ArrayList<TreeNode<T>>();
 		}
 	}*/
-    public  static boolean isPresent = false;
+    	public  static boolean isPresent = false;
 	
 	public static boolean checkIfContainsX(TreeNode<Integer> root, int x){
 
-		// Write your code here
+	// Write your code here
         if (root == null){
-        	isPresent = false;
+           isPresent = false;
             }
         else if(root.data == x){
-			return true;
-		}
-		for (int i = 0; i < root.children.size(); i++){
-			isPresent = checkIfContainsX(root.children.get(i), x);
-		}
+	   return true;
+	}
+	for (int i = 0; i < root.children.size(); i++){
+	    isPresent = checkIfContainsX(root.children.get(i), x);
+	}
         return isPresent;
 
 	}
